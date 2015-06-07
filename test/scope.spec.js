d1cf0b4d9cffa80aece67d665b16815030ba75c5
Ashish Dasnurkar <kachhalimbu@gmail.com>
2015-06-07 11:42:30 +0800

    implemented

--- test/scope.spec.js
+++ test/scope.spec.js
@@ -233,5 +233,30 @@ describe('test Scope', function() {
       expect(result).toBe(42);
     });
 
+
+    it('should $apply function and start digest cycle', function() {
+      scope.aValue = 'someValue';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) {
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.$apply(function(scope) {
+        scope.aValue = 'someOtherValue';
+      });
+
+      expect(scope.counter).toBe(2);
+
+    })
+
   });
 });
